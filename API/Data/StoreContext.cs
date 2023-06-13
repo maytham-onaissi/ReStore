a90be691d059b84bede81d0227929542c46dccc8
Maytham Onaissi <MaythamOnaissi_Business@hotmail.com>
2023-06-13 19:32:11 +0300

    End of section 2

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+  public class StoreContext : DbContext
+  {
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    // This basically creates a database of the type Product, which was made in ./Entities/Product
+    public DbSet<Product> Products { get; set; }
+  }
+}
---
+++ API/Program.cs
@@ -0,0 +1,50 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// This part basically enables the app to use the database from everywhere.
+// check appsettings.Development.json to understand more of it.
+builder.Services.AddDbContext<StoreContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// This basically creates a database via code. instead of the dotnet CLI.
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    context.Database.Migrate();
+    // Data/Migrations/DbInitializer.
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "A problem occured during migration");
+     
+}
+app.Run();
